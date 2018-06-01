95c21429c3d7c6c9c051b8683f22e8a755ff7df3
Ganesh Tikone <Ganesh.Tikone@Silicus.com>
2018-06-01 11:22:12 +0530

    #CrashCode

--- app/src/main/java/com/mobilepractice/silicus/vsacdemo/SplashActivity.java
+++ app/src/main/java/com/mobilepractice/silicus/vsacdemo/SplashActivity.java
@@ -2,12 +2,33 @@ package com.mobilepractice.silicus.vsacdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SplashActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-    }
+  private Button buttonCrashMe;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash);
+
+    setListener();
+  }
+
+  /**
+   * Crash Application onClick
+   */
+  private void setListener() {
+
+    buttonCrashMe.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Log.d("####", "Crash Me ");
+      }
+    });
+  }
 }
